279f48def8fbc1c596051b92fc695a0f446d5bdf
Kevin Bluer <kevin@bluer.com>
2013-07-30 08:19:28 -0400

    initial upload

---
+++ arrays/arrays/js/exercise.js
@@ -0,0 +1,42 @@
+/**
+ * JavaScript Exercise: Data Types
+ * Most of your answers should be stored in variables called q1, q2 etc.. and the variables printed to the console.
+ 
+ 	(i.e) console.log("Question 1" + q1)
+ */
+
+/**
+ * Question 1
+ * Create an array with the elements 'New York' 'London' 'San Francisco'
+ */
+var q1 = 'REPLACE_ME'
+
+/**
+ * Question 2
+ * Create an array with the elements 'New York' 'London' 'San Francisco' and extract the first element
+ */
+var q2 = 'REPLACE_ME'
+
+/**
+ * Question 3
+ * Create an array with the elements 'New York' 'London' 'San Francisco' and extract the last element
+ */
+var q3 = 'REPLACE_ME'
+
+/**
+ * Question 4
+ * Get the length of var q3
+ */
+var q4 = 'REPLACE_ME'
+
+/**
+ * Question 5
+ * Step 1: Create a new array called q5 elements 'New York' 'London' 'San Francisco'. 
+ * Step 2: Using a method, add 'Berlin', 'Sydney', 'Boston',  'LA' to q5 array.
+ */
+ var q5 = 'REPLACE_ME'
+/**
+ * Question 6
+ * Using a method, get  the length of array q5.
+ */
+var q6 = 'REPLACE_ME'
